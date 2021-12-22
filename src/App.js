dba1e45f558211e1f0542e66adc7b408c4e44f96
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-22 20:17:28 +0530

    completed feedback-stats component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} />
       </div>
     </>
