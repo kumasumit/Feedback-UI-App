74f4f1d57c65bc6db22937be4147e4825da63e08
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-23 09:56:01 +0530

    Completed Button Component Validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,7 +5,20 @@ import Card from "./shared/Card";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
   const handleTextChange = (evt) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be at least 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(evt.target.value);
   };
   return (
@@ -21,8 +34,11 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit">Send me</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send me
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
