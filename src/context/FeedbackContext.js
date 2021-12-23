6652c32375adc1e69ec3d8a61d4d2bd29c100955
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-23 18:08:07 +0530

    Update feedback Item

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -35,14 +35,23 @@ export const FeedbackProvider = ({ children }) => {
       edit: true,
     });
   };
+
+  // Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit,
+        updateFeedback,
       }}
     >
       {children}
