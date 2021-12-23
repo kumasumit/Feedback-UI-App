9f346c774a51ebe01b1169b4be449b1057dd3152
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-23 11:14:04 +0530

    Added Feedback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
@@ -7,11 +8,15 @@ import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} />
       </div>
